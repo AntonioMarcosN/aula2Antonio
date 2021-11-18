ee67c0e7d91030f7681842f760f9ee3a2df5f9a0
AntonioMarcosN <antoniomarcosn6@gmail.com>
2021-11-18 18:37:48 -0300

    exercicio do JSON

---
+++ aula3/exercicioJson.js
@@ -0,0 +1,92 @@
+[
+    {
+        "id": 1,
+        "carro": "Voyage",
+        "Tipo": "sedan",
+        "fabricante": "VW",
+        "cor": "branca",
+        "novo": false,
+        "motor": "1.4"       
+    },
+    {
+        "id": 2,
+        "carro": "Gol",
+        "Tipo": "hatch",
+        "fabricante": "VW",
+        "cor": "preta",
+        "novo": false,
+        "motor": "1.0"     
+    },
+    {
+        "id": 3,
+        "carro": "Fiesta",
+        "Tipo": "sedan",
+        "fabricante": "Ford",
+        "cor": "branca",
+        "novo": false,
+        "motor": "1.0"    
+    },
+    {
+        "id": 4,
+        "carro": "Sienna",
+        "Tipo": "sedan",
+        "fabricante": "Fiat",
+        "cor": "prata",
+        "novo": false,
+        "motor": "1.4"   
+    },
+    {
+        "id": 5,
+        "carro": "Palio",
+        "Tipo": "hatch",
+        "fabricante": "Fiat",
+        "cor": "prata",
+        "novo": true,
+        "motor": "1.0"    
+    },
+    {
+        "id": 6,
+        "carro": "Ka",
+        "Tipo": "hatch",
+        "fabricante": "Ford",
+        "cor": "branca",
+        "novo": false,
+        "motor": "1.0"    
+    },
+    {
+        "id": 7,
+        "carro": "Fiesta",
+        "Tipo": "hatch",
+        "fabricante": "Ford",
+        "cor": "vermelho",
+        "novo": false,
+        "motor": "1.6"    
+    },
+    {
+        "id": 8,
+        "carro": "Prisma",
+        "Tipo": "sedan",
+        "fabricante": "Chevrolet",
+        "cor": "branca",
+        "novo": false,
+        "motor": "1.4"    
+    },
+    {
+        "id": 9,
+        "carro": "Onix",
+       "Tipo": "hatch",
+        "fabricante": "Chevrolet",
+        "cor": "preto",
+        "novo": false,
+        "motor": "1.0"    
+    },
+    {
+        "id": 10,
+        "carro": "Sandeiro",
+        "Tipo": "hatch",
+        "fabricante": "Renault",
+        "cor": "prata",
+        "novo": true,
+        "motor": "1.4"    
+    },
+]
